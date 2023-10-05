a9a20bc883ffb0d026632b666fa889f3e0c64270
SKhrebtan <shrebtan@gmail.com>
2023-10-05 15:22:38 +0300

    fail test

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,7 +1,8 @@
 const { ctrlWrapper } = require("../../helpers");
 const path = require('path');
-const fs = require("fs/promises");
+// const fs = require("fs/promises");
 const { User } = require("../../models/user");
+const jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars")
 
@@ -10,9 +11,13 @@ const updateAvatar = async (req, res) => {
     const { path: tempUpload, originalname } = req.file;
     const filename = `${_id}_${originalname}`;
     const resultUpload = path.join(avatarsDir, filename);
-    await fs.rename(tempUpload, resultUpload);
+    // await fs.rename(tempUpload, resultUpload);
     const avatarURL = ("avatars", filename);
     await User.findByIdAndUpdate(_id, { avatarURL });
+    // const image = await jimp.read(resultUpload);
+     const image = await jimp.read(tempUpload);
+    await image.resize(250, 250, jimp.HORIZONTAL_ALIGN_CENTER);
+    await image.writeAsync(resultUpload);
 
     res.json({
         avatarURL,
